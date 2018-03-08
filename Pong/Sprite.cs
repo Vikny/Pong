106269b789157e695228de7a549f8bdb6e000054
Vikny <37171798+Vikny@users.noreply.github.com>
2018-03-08 16:48:37 +0700

    Initial commit

---
+++ Pong/Sprite.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Pong
+{
+   class Sprite
+   {
+      public Vector2 Position { get; set; }
+      public Texture2D Texture { get; set; }
+      public SpriteBatch SpriteBatch { get; set; }
+
+      public Sprite(Texture2D texture, Vector2 position, SpriteBatch spriteBatch)
+      {
+         Texture = texture;
+         Position = position;
+         SpriteBatch = spriteBatch;
+      }
+
+      public virtual void Draw()
+      {
+         SpriteBatch.Draw(Texture, Position, Color.Green);
+      }
+   }
+}
